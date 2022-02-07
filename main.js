d8ffbaa00ab4a1ad6b4bf09400d45acb76b952f5
Daniel <rhkdqhr90@gami.com>
2022-02-07 14:27:54 +0900

    js: Nabarmenu click

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 'use strict';
-
+//Make navbar
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
-    console.log(window.scrollY);
-    console.log(`navbar height ${navbarHeight}`);
+    
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
 
     }else{
         navbar.classList.remove('navbar--dark');
     }
+});
+
+
+// Hansdle scrolling when taping on the navbar menu
+
+const nav__menu = document.querySelector('.nav__menu');
+
+nav__menu.addEventListener('click',(event)=>{    
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+   
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+
+    
 });
