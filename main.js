9a6faf7262df94700280ebceef4bf639d00b1dfa
Daniel <rhkdqhr90@gami.com>
2022-02-07 16:11:32 +0900

    JS: transparent

--- main.js
+++ main.js
@@ -23,9 +23,28 @@ nav__menu.addEventListener('click',(event)=>{
     if (link == null){
         return;
     }
-   
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollInToView(link);
 
     
 });
+// handle click on "contact me " button on me
+const contact = document.querySelector('.home__contact');
+
+contact.addEventListener('click',(event)=>{
+    scrollInToView('#contact')
+});
+
+function scrollInToView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
+// make home slowy fade to transpratarent as the window scrolls down
+
+const home = document.querySelector(".home__container");
+const homeheigh = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1-window.scrollY/homeheigh;
+   
+});
+
