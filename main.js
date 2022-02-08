9b8291d226f52c9040fe58b9ddf5f8c187e8138c
Daniel <rhkdqhr90@gami.com>
2022-02-08 13:51:58 +0900

    JS: Toggle

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.addEventListener('scroll',()=>{
 });
 
 
+
 // Hansdle scrolling when taping on the navbar menu
 
 const nav__menu = document.querySelector('.nav__menu');
@@ -23,9 +24,16 @@ nav__menu.addEventListener('click',(event)=>{
     if (link == null){
         return;
     }
+    nav__menu.classList.remove('open');
     scrollInToView(link);
 
-    
+});
+//Nabar toffle button for small screen 
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+    console.log('dd');
+    nav__menu.classList.toggle('open');
 });
 // handle click on "contact me " button on me
 const contact = document.querySelector('.home__contact');
@@ -42,7 +50,7 @@ const home = document.querySelector(".home__container");
 const homeheigh = home.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
     home.style.opacity = 1-window.scrollY/homeheigh;
-   
+
 });
 //Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
@@ -58,6 +66,47 @@ document.addEventListener("scroll", () => {
 arrowUp.addEventListener("click", ()=>{
     scrollInToView('#home');
 });
+
+// My work 
+const workBtn = document.querySelector('.work__categories');
+const workProject = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtn.addEventListener('click', (event)=>{
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+    if(filter == null){
+        return
+    }
+    //Remove selection form the previous item and select the new
+    const active = document.querySelector('.category_btn.selected');
+    active.classList.remove('selected');
+    const target = event.target.nodeName === 'BUTTON' ? event.target : 
+    event.target.parentNode;
+    target.classList.add('selected');
+    
+    workProject.classList.add('ainim-out');   
+    setTimeout(()=>{
+        projects.forEach((project)=>{        
+            if(filter ==='*' || filter === project.dataset.type) {
+                project.classList.remove('invisibe');
+            }else{
+                project.classList.add('invisibe');
+            }
+        });
+        workProject.classList.remove('ainim-out');
+    },300)
+
+ /* for(let project of projects){
+        console.log(project);
+    }
+
+    let project
+    for(let i = 0;i <projects.length ; i++){
+        project = projects[i];
+        console.log(project);
+    }*/
+});
+
 
 function scrollInToView(selector){
     const scrollTo = document.querySelector(selector);
